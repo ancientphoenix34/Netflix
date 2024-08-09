1b3a6e6d74970feb72ef41d1f4f641988d7c0f9c
ancientphoenix34 <farsukoothradan@gmail.com>
2024-08-09 23:02:07 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAvN4vGolAFB4eFtZKbdDvgkflSgxV-20g",
+    authDomain: "netflix-clone-27-f9b5b.firebaseapp.com",
+    projectId: "netflix-clone-27-f9b5b",
+    storageBucket: "netflix-clone-27-f9b5b.appspot.com",
+    messagingSenderId: "443584075811",
+    appId: "1:443584075811:web:31049afeef6e8817d5f996",
+    measurementId: "G-R9VF0CL0SP"
+  };
