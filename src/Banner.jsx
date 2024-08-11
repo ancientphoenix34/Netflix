f05e179421ed18e85cdb87466f6ec4e631b6e75e
ancientphoenix34 <farsukoothradan@gmail.com>
2024-08-11 19:08:54 +0530

    API integration done

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
-
+import axios from './axios';
+import requests from './Requests';
 const Banner = () => {
 
+  const[movie,setMovie]=useState([]);
+
+  useEffect(()=>{
+    async function fetchData(){
+    const request=await axios.get(requests.fetchNetflixOriginals);
+    setMovie(
+      request.data.results[
+        Math.floor(Math.random()*request.data.results.length-1)
+      ]
+    );
+    return request;
+    }
+    fetchData();
+  },[])
+  console.log(movie)
 
   const truncate=(string,n)=>{
 return string?.lenth>n?string.substr(0,n-1)+'...':string
